9fba7e742ef97d10a5cc3f549b1e51f45f18d082
allimak55 <147518756+allimak55@users.noreply.github.com>
2024-10-15 19:14:26 +0300

    Update Firstproject.java

--- Firstproject.java
+++ Firstproject.java
@@ -1,1 +1,15 @@
-
+import java.io.PrintStream;
+import java.util.Scanner;
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = System.out;
+    public static void main(String[] args) {
+        int num = in.nextInt();
+        String rem = "";
+        while(num != 0) {
+            rem = num % 2 + rem;
+            num /= 2;
+        }
+        out.print(rem);
+    }
+}
